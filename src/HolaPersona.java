e2158b77cf9c04e80748f74b83abb9fb6ca393e2
mcasas21 <mariocasas21@gmail.com>
2023-10-29 11:06:46 +0100

    Persona

---
+++ src/HolaPersona.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HolaPersona {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		
+		Scanner sc=new Scanner(System.in);
+	
+		String nombre;
+		
+		System.out.println("Dame tu nombre:");
+		
+		nombre=sc.next();
+		
+		System.out.println("Hola " + nombre);
+		
+		sc.close();
+
+	}
+
+}
