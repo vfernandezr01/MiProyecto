a6e6286e08ab1bee92d96222c1e221f5396c893f
vfernandezr01 <vfernandezr01@educarex.es>
2023-11-23 14:14:05 +0100

    forkMod

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -7,7 +7,7 @@ public class HolaMundo {
 		System.out.println("Hola Mundo");
 		System.out.println("Bienvenidos...");
 		System.out.println("La suma de 7 más 2 es igual a: " + (7+2));
-
+		System.out.println("Victor");
 	}
 
 }
